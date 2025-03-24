2fe7041c6919e92794c0626add98dbe198b4f28b
tamakoma <tamakoma0125@gmail.com>
2025-03-24 23:20:01 +0900

    [fix] PostFinishTest.phpをもう少し補強

--- tests/Feature/Http/Controllers/MediaFileController/tusdHooks/PostFinishTest.php
+++ tests/Feature/Http/Controllers/MediaFileController/tusdHooks/PostFinishTest.php
@@ -1,26 +1,43 @@
 <?php
 
 
+use App\Jobs\TusdHooks\PostFinish\ProcessAudio;
+use App\Jobs\TusdHooks\PostFinish\ProcessImage;
+use App\Jobs\TusdHooks\PostFinish\ProcessManga;
+use App\Jobs\TusdHooks\PostFinish\ProcessVideo;
+
 use function Pest\Laravel\postJson;
 
-test('200レスポンスが帰ってきて、queueにジョブも入ってる', function () {
+test('200レスポンスが帰ってきて、queueにジョブも入ってる', function (
+    string $fileName,
+    string $mimeType,
+    string $type,
+    string $jobClass
+) {
     Queue::fake();
 
     login();
     $infoFileName = Str::random(32) . ".info";
     $payload = postFinishPayload(
-        "image.png",
-        "image/png",
+        $fileName,
+        $mimeType,
         10000,
-        "/private/uploads/images/image.png",
+        "/private/uploads/$type/$fileName",
         "/private/$infoFileName"
     );
 
     $response = postJson(route("tusd-hooks"), $payload);
 
     $response->assertOk();
+    Queue::assertPushed($jobClass);
     Queue::assertCount(1);
-});
+})
+    ->with([
+        ["image.png", "image/png", "images", ProcessImage::class],
+        ["audio.mp3", "audio/mpeg", "audios", ProcessAudio::class],
+        ["video.mp4", "video/mp4", "videos", ProcessVideo::class],
+        ["manga.zip", "application/zip", "mangas", ProcessManga::class],
+    ]);
 
 test('ゲストだと認証エラーかつqueueにジョブが入らない', function () {
     Queue::fake();
