065bb9f173767868031a20cb4a0297a92167afd2
tamakoma <tamakoma0125@gmail.com>
2025-03-27 00:13:59 +0900

    [add] EditMenuBar.vueを追加し、そこからメディアの削除と全選択の解除をできるように

---
+++ resources/js/Pages/Media/Index/Partials/EditMenuBar.vue
@@ -0,0 +1,43 @@
+<script setup>
+import ToolTip from "@/Components/ToolTip.vue";
+import {useMediaEditStore} from "@/stores/mediaEdit.js";
+import {useMediaList} from "@/stores/mediaList.js";
+import {ref} from "vue";
+import DeleteEdit from "@/Pages/Media/Index/Partials/DeleteEdit.vue";
+
+const mediaList = useMediaList();
+const mediaEditStore = useMediaEditStore();
+const isOpenActionMenu = ref(false);
+const deleteEditRef = ref(null);
+</script>
+
+<template>
+    <div
+        class="fixed bottom-104 inset-x-0 max-w-max mx-auto bg-white/65 rounded-xl shadow-black shadow-[0px_0px_12px] py-8 px-24 min-h-40 min-w-168 flex items-center justify-center">
+        <p class="select-none">{{ mediaEditStore.selectedMediaIds.length }} / {{ mediaList.mediaList.length }} 選択中</p>
+        <ToolTip message="全ての選択を解除" class="ml-16">
+            <button class="flex items-center hover:bg-sumi-200 rounded-full p-4"
+                @click="mediaEditStore.clearSelection">
+                <i-hugeicons-check-unread-04 class="w-24 h-24"/>
+            </button>
+        </ToolTip>
+        <div class="flex items-center ml-4 relative">
+            <button @click="isOpenActionMenu = !isOpenActionMenu"
+                    v-click-outside="() => isOpenActionMenu = false"
+                    class="hover:bg-sumi-200 rounded-full p-4">
+                <i-iconamoon-menu-kebab-vertical class="w-24 h-24"/>
+            </button>
+            <div class="absolute bottom-40 bg-white/75 flex flex-col items-center rounded-xl shadow-black shadow-[0px_0px_12px] py-16"
+                 v-if="isOpenActionMenu">
+                <button
+                    class="w-full py-4 pl-8 pr-16 whitespace-nowrap hover:bg-teto-200 hover:text-teto-800 flex items-center justify-center gap-x-16 select-none"
+                    @click.stop="deleteEditRef.openDeleteDialog">
+                    <i-bytesize-trash class="h-full w-auto" />
+                    メディアを削除
+                </button>
+            </div>
+        </div>
+    </div>
+
+    <DeleteEdit ref="deleteEditRef" />
+</template>
